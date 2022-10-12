35b7dcd5a29c83db2e3bc85e22f4f5a39db1948c
codesoap <codesoap@mailbox.org>
2022-10-12 20:29:34 +0200

    Update nodes in configs

--- cmd/atto-safesign/config.go
+++ cmd/atto-safesign/config.go
@@ -1,13 +1,10 @@
 package main
 
 var (
-	// The node needs to support the work_generate action.
-	// See e.g. https://publicnodes.somenano.com to find public nodes or
-	// set up your own node to use. Here are some public alternatives:
-	// - https://proxy.nanos.cc/proxy
-	// - https://mynano.ninja/api/node
-	// - https://rainstorm.city/api
-	node = "https://proxy.powernode.cc/proxy"
+	// The node needs to support the work_generate action. See e.g.
+	// https://publicnodes.somenano.com to find public nodes or set up your
+	// own node. One public alternative would be https://rainstorm.city/api
+	node = "https://proxy.nanos.cc/proxy"
 
 	// defaultRepresentative will be set as the representative when
 	// opening an accout, but can be changed afterwards. See e.g.
