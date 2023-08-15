5462b6b87dbd0a459d0f6d0781b241065dc776e1
codesoap <codesoap@mailbox.org>
2023-08-15 07:40:11 +0200

    Update node and link to representative overview
    Many nodes as well as https://mynano.ninja seem to have gone offline.

--- cmd/atto-safesign/config.go
+++ cmd/atto-safesign/config.go
@@ -1,13 +1,13 @@
 package main
 
 var (
-	// The node needs to support the work_generate action. See e.g.
-	// https://publicnodes.somenano.com to find public nodes or set up your
-	// own node. One public alternative would be https://rainstorm.city/api
-	node = "https://proxy.nanos.cc/proxy"
+	// The node needs to support the work_generate action. See
+	// e.g. https://publicnodes.somenano.com to find public nodes
+	// or set up your own node.
+	node = "https://rainstorm.city/api"
 
 	// defaultRepresentative will be set as the representative when
 	// opening an accout, but can be changed afterwards. See e.g.
-	// https://mynano.ninja/principals to find representatives.
+	// https://nanolooker.com/representatives to find representatives.
 	defaultRepresentative = "nano_1jtx5p8141zjtukz4msp1x93st7nh475f74odj8673qqm96xczmtcnanos1o"
 )
